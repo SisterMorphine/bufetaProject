4029e34a3bc794276294f6197a48c11ff9974524
SisterMorphine <leti.rocha@gmail.com>
2025-10-27 20:24:18 +0100

    Testing - Add library and the first widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:catproject/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets("My cat widget has title", (WidgetTester widgetTester) async {
+    await widgetTester.pumpWidget(const MyApp());
+
+    final titleFinder = find.text('Random Cat Viewer 🐱');
+
+    expect(titleFinder, findsOneWidget);
+  });
+}
